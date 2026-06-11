1d1be8b6696662f5c2a28938729f3698badf55cf
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-06-11 21:42:27 +0530

    solved 1862B

---
+++ 800/1862B_Sequence_Game.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> b(n);
+        for (int i = 0; i < n; i++) {
+            cin >> b[i];
+        }
+
+        vector<int> a;
+        a.push_back(b[0]);
+
+        for (int i = 0; i < n - 1; i++) {
+            if (b[i] <= b[i + 1]) {
+                a.push_back(b[i + 1]);
+            } else {
+                a.push_back(1);
+                a.push_back(b[i + 1]);
+            }
+        }
+
+        cout << a.size() << endl;
+        for (int x : a) {
+            cout << x << " ";
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
