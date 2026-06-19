8f740db8f4ff38bb7cb75efdb3e8e2299d5412cb
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-06-19 19:52:05 +0530

    Solved 1783A Make it Beautiful

---
+++ 800/1783A_Make_it_Beautiful.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0; i<n; i++){
+            cin>>a[i];
+        }
+        sort(a.begin(), a.end());
+        if(a[0]==a[n-1] ) cout<<"NO"<<endl;
+        else{
+            cout<<"YES"<<endl;
+            cout<<a[n-1]<<" " ;
+            for(int i=0; i<n-1;i++){
+                cout<<a[i]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+}
