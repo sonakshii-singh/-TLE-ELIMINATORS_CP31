54bc6f606b20ec5704ec5ac5e8ea6eb760e6cf72
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-06-20 18:40:37 +0530

    Solved 1761A

--- 800/1805A_We_Need_the_Zero.cpp
+++ 800/1805A_We_Need_the_Zero.cpp
@@ -7,11 +7,24 @@ int main() {
 
     while (t--) {
         int n;
-        cin>>n;
-        vector<int> a(n);
-        for(int i=0; i<n; i++){
-              cin>>a[i];
+        cin >> n;
+
+        int xr = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            xr ^= x;
         }
-       
+
+        if (n % 2 == 1) {
+            cout << xr << endl;
+        }
+        else {
+            if (xr == 0) cout << 0 << endl;
+            else cout << -1 << endl;
+        }
     }
+
+    return 0;
 }
