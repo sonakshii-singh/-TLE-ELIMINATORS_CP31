e7b5a560a2120229e8578d8d2d4fb616da523e94
sonakshii-singh <sonakshisinghpatel@gmail.com>
2026-07-16 23:34:22 +0530

    Solved Comparison String

---
+++ 900/1837B_Comparison_String.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        int ans = 1;
+        int cnt = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (s[i] == s[i - 1])
+                cnt++;
+            else
+                cnt = 1;
+
+            ans = max(ans, cnt);
+        }
+
+        cout << ans + 1 << '\n';
+    }
+}
